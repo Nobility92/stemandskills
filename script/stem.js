762718d8d76b347581adaeed2d2667788df7215b
Nobility92 <nwa21004@byui.edu>
2022-07-21 14:05:39 +0100

    c

--- script/stem.js
+++ script/stem.js
@@ -2,5 +2,12 @@ const d = new Date();
 let datetime = d.getFullYear()+'/'+d.getMonth()+'/'+d.getDate()+ '  ' + d.getHours()+':'+d.getMinutes()+':'+d.getSeconds();
 document.getElementById("update").innerHTML = datetime;
 
-//for weather diplay
-const url = "https://api.openweathermap.org/data/2.5/onecall?lat=36.97820922794823&lon=-109.1270983583244&exclude=hourly,minutely&appid=063dbb4eea74383c891a7e335525315b";
+//for hamburger Menu
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
