0d336fa755e7624206bfebaef05ee7f153facf8a
Nobility92 <nwa21004@byui.edu>
2022-07-21 04:35:41 +0100

    c

--- script/stem.js
+++ script/stem.js
@@ -1,3 +1,6 @@
 const d = new Date();
 let datetime = d.getFullYear()+'/'+d.getMonth()+'/'+d.getDate()+ '  ' + d.getHours()+':'+d.getMinutes()+':'+d.getSeconds();
 document.getElementById("update").innerHTML = datetime;
+
+//for weather diplay
+const url = "https://api.openweathermap.org/data/2.5/onecall?lat=36.97820922794823&lon=-109.1270983583244&exclude=hourly,minutely&appid=063dbb4eea74383c891a7e335525315b";
