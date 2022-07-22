5b0b88400049e21fe29b9e3bd5155daf82234b53
Nobility92 <nwa21004@byui.edu>
2022-07-22 08:06:57 +0100

    c

--- script/stem.js
+++ script/stem.js
@@ -11,3 +11,22 @@ function myFunction() {
       x.className = "topnav";
     }
   }
+  //for like button
+  const likeBtn = document.querySelector(".like__btn");
+let likeIcon = document.querySelector("#icon"),
+  count = document.querySelector("#count");
+
+let clicked = false;
+
+
+likeBtn.addEventListener("click", () => {
+  if (!clicked) {
+    clicked = true;
+    likeIcon.innerHTML = `<i class="fas fa-thumbs-up"></i>`;
+    count.textContent++;
+  } else {
+    clicked = false;
+    likeIcon.innerHTML = `<i class="far fa-thumbs-up"></i>`;
+    count.textContent--;
+  }
+});
